476a5ea4be8b5f5dfe16cc88bff64495b1d4e790
Ludvig Sterky <Ludvig.Sterky@elev.ga.ntig.se>
2024-09-06 13:33:07 +0200

    Fråge

--- Frågesport Happy/Program.cs
+++ Frågesport Happy/Program.cs
@@ -10,7 +10,7 @@ string fråga1 = "";
 System.Console.WriteLine("Välkommen till frågesport. Är du redo?");
 svar = Console.ReadLine();
  if(svar == "ja" || svar == "Ja"){
-    System.Console.WriteLine("Du kommer att få svara på 4 frågor. Om du svara rätt på alla vinner du 20000$!".);
+    System.Console.WriteLine("Du kommer att få svara på 4 frågor. Om du svara rätt på alla vinner du 20000$!");
     System.Console.WriteLine("Fråga 1, Vad är carbonara?");
 
  }
