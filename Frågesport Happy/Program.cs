776568cdc7003e4a6e95abddc8024b1b4b38a421
Ludvig Sterky <Ludvig.Sterky@elev.ga.ntig.se>
2024-09-09 08:52:20 +0200

    happy

--- Frågesport Happy/Program.cs
+++ Frågesport Happy/Program.cs
@@ -1,16 +1,15 @@
-﻿using System.Globalization;
-
-Console.BackgroundColor = ConsoleColor.Blue;
+﻿Console.BackgroundColor = ConsoleColor.Blue;
 Console.ForegroundColor = ConsoleColor.Yellow;
 Console.Clear();
 
-string svar = "";
+string Startsvar = "";
 string fråga1 = "";
+int poäng;
 
 System.Console.WriteLine("Välkommen till frågesport. Är du redo?");
-svar = Console.ReadLine();
- if(svar == "ja" || svar == "Ja"){
+Startsvar = Console.ReadLine();
+ if(Startsvar == "ja" || Startsvar == "Ja"){
     System.Console.WriteLine("Du kommer att få svara på 4 frågor. Om du svara rätt på alla vinner du 20000$!");
-    System.Console.WriteLine("Fråga 1, Vad är carbonara?");
+    System.Console.WriteLine("Fråga 1, Vad är en fluga?");
 
  }
